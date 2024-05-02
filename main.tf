2aa62337099e6aee553e02ae497084786ae364d9
safvan <safvan@gmail.com>
2024-05-02 19:00:12 +0000

    key pair

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_key_pair" "auth_key" {
+
+  key_name   = "${var.project_name}-${var.project_env}"
+  public_key = file("mykey.pub")
+  tags = {
+    Name = "${var.project_name}-${var.project_env}"
+
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+
+  description = "terraform project name"
+  type        = string
+  default     = "zomato"
+}
+
+
+variable "project_env" {
+
+  description = "terraform project environment"
+  type        = string
+  default     = "production"
+}
